530eef1b8451ac39bc52111e8cfad6dd191d47cb
Wadabee <bh.jpvw@gmail.com>
2022-02-20 21:54:43 +0900

    change browse router to hash router

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { PokerProvider } from "./providers/poker";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <PokerProvider>
       <React.StrictMode>
         <App />
       </React.StrictMode>
     </PokerProvider>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
 
