a717cd60460af4959c41ec06216e98708bd54407
Wadabee <bh.jpvw@gmail.com>
2022-02-23 00:59:58 +0900

    add pie chart and point by mean

--- src/components/ChartPokerResult.tsx
+++ src/components/ChartPokerResult.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useMemo } from "react";
+import _ from "lodash";
 
 import ReactECharts from "echarts-for-react";
 import { EChartsOption } from "echarts";
@@ -11,21 +12,41 @@ type Props = {
 };
 
 const ChartPokerResult: React.FC<Props> = ({ data }) => {
+  const pieData = useMemo(() => {
+    return Object.keys(data).map((key) => ({
+      name: key,
+      value: data[key],
+    }));
+  }, [data]);
+
+  const mean = useMemo(() => {
+    const values: number[] = [];
+    Object.keys(data).forEach((key) => {
+      values.push(..._.fill(Array(data[key]), Number.parseInt(key)));
+    });
+    return _.mean(values);
+  }, [data]);
+
   const options: EChartsOption = {
     title: {
       text: "結果",
     },
-    yAxis: {
-      type: "category",
-      data: Object.keys(data),
-      name: "ポイント",
-    },
-    xAxis: {
-      name: "選択者数",
-      type: "value",
-      nameLocation: "middle",
-      nameGap: 20,
-    },
+    grid: [{ right: "55%" }],
+    yAxis: [
+      {
+        type: "category",
+        data: Object.keys(data),
+        name: "ポイント",
+      },
+    ],
+    xAxis: [
+      {
+        name: "選択者数",
+        type: "value",
+        nameLocation: "middle",
+        nameGap: 20,
+      },
+    ],
     series: [
       {
         data: Object.values(data),
@@ -35,11 +56,52 @@ const ChartPokerResult: React.FC<Props> = ({ data }) => {
           color: "rgba(180, 180, 180, 0.2)",
         },
       },
+      {
+        type: "pie",
+        radius: [0, "30%"],
+        center: ["75%", "50%"],
+        label: {
+          position: "center",
+          fontSize: 25,
+        },
+        labelLine: {
+          show: false,
+        },
+        silent: true,
+        data: [
+          {
+            value: 0,
+            name: `平均値\n${mean}`,
+            itemStyle: {
+              color: "rgba(255, 255, 255, 1)",
+            },
+          },
+        ],
+      },
+      {
+        name: "Access From",
+        type: "pie",
+        radius: ["40%", "70%"],
+        center: ["75%", "50%"],
+        data: pieData,
+        label: {
+          position: "inside",
+          formatter: "{b}\n{per|{d}%}",
+          rich: {
+            per: {
+              color: "#fff",
+              backgroundColor: "#4C5058",
+              padding: [3, 4],
+              borderRadius: 4,
+            },
+          },
+        },
+      },
     ],
   };
 
   return (
-    <Card sx={{ minWidth: "500px" }}>
+    <Card sx={{ minWidth: "700px" }}>
       <CardContent>
         <ReactECharts
           option={options}
