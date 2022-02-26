fe6ca78c625c4e5669b326a16d70a8582cd9dc73
Wadabee <bh.jpvw@gmail.com>
2022-02-27 01:57:53 +0900

    add database url in actions yaml

--- src/firebase/firestore.ts
+++ src/firebase/firestore.ts
@@ -16,8 +16,6 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID,
 };
 
-console.log(firebaseConfig.databaseURL);
-
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const rtdb = getDatabase(app);
